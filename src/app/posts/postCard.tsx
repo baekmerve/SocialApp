2a3fa54398239ed048ecdf8e947d06b88d7d6d49
baekmerve <baekmerve@gmail.com>
2025-03-13 04:40:37 +0900

    seperated post content for better ui

--- src/app/posts/postCard.tsx
+++ src/app/posts/postCard.tsx
@@ -1,6 +1,5 @@
 import { formatDistanceToNow } from "date-fns";
 import Link from "next/link";
-import Image from "next/image";
 import {
   Card,
   CardContent,
@@ -13,6 +12,7 @@ import DeleteItem from "@/components/deleteItem";
 import { Avatar, AvatarImage } from "@/components/ui/avatar";
 import { deletePost } from "@/actions/postActions";
 import PostFooter from "@/components/post/postFooter";
+import PostContent from "@/components/post/postContent";
 
 interface Props {
   post: Post;
@@ -30,7 +30,7 @@ export default function TestPostCard({
   const isLoggedIn = Boolean(currentUserId);
 
   return (
-    <Card className="relative mx-auto w-[90%] overflow-hidden rounded-2xl shadow-lg border-2 dark:shadow-shadow px-4">
+    <Card className="relative mx-auto w-[90%] overflow-hidden rounded-2xl shadow-lg border-2 dark:shadow-shadow px-4 max-h-[600px] gap-2 py-3">
       {/* Overlay for blur effect and lock icon */}
       {!isLoggedIn && (
         <div className="absolute inset-0 flex  flex-col gap-2 items-center justify-center bg-black/40  bg-bg/50 backdrop-blur-sm z-10">
@@ -41,14 +41,14 @@ export default function TestPostCard({
         </div>
       )}
       {/* POST HEADER */}
-      <CardHeader className="px-6 py-3 border-b border-gray-200 dark:border-gray-700">
+      <CardHeader className="px-6 py-3 border-b-2 ">
         <div className="flex items-center justify-between">
           {/* User Info */}
           <Link
             href={`/profile/${post.author.username}`}
             className="flex items-center gap-4 group"
           >
-            <Avatar className="w-12 h-12 border-2  ">
+            <Avatar className="w-12 h-12 border-2 ">
               <AvatarImage
                 src={post.author.image ?? "/avatar.avif"}
                 alt="User's avatar"
@@ -81,45 +81,11 @@ export default function TestPostCard({
       </CardHeader>
 
       {/* POST CONTENT */}
-      {useLink ? (
-        <Link href={`/posts/${post.id}`} className="block">
-          <CardContent className="py-1 px-4 space-y-3">
-            <p className="text-lg font-semibold break-words">{post.title}</p>
-            <p className="text-sm break-words">{post.content}</p>
+      <CardContent className=" px-4  ">
+        <PostContent post={post} useLink={useLink} />
+      </CardContent>
 
-            {post.image && (
-              <div className="relative mx-auto w-full max-w-[600px] max-h-[300px] overflow-hidden border-2 shadow-md hover:shadow-lg transition-shadow duration-200 rounded-2xl">
-                <Image
-                  src={post.image}
-                  alt="Post content"
-                  width={200}
-                  height={200}
-                  className="w-full h-auto object-cover"
-                  priority
-                />
-              </div>
-            )}
-          </CardContent>
-        </Link>
-      ) : (
-        <CardContent className="py-1 px-4 space-y-3">
-          <p className="text-lg font-semibold break-words">{post.title}</p>
-          <p className="text-sm break-words">{post.content}</p>
-
-          {post.image && (
-            <div className="relative mx-auto w-full max-w-[600px] max-h-[300px] overflow-hidden border-2 shadow-md hover:shadow-lg transition-shadow duration-200 rounded-2xl">
-              <Image
-                src={post.image}
-                alt="Post content"
-                width={200}
-                height={200}
-                className="w-full h-auto object-cover"
-              />
-            </div>
-          )}
-        </CardContent>
-      )}
-      <CardFooter className="flex flex-col px-4  ">
+      <CardFooter className="flex flex-col px-4  py-0 ">
         <PostFooter
           postId={post.id}
           initialLiked={post.likes.some(
---
+++ src/components/post/postContent.tsx
@@ -0,0 +1,59 @@
+"use client";
+import { useState } from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { Post } from "@/types/type";
+
+interface Props {
+  post: Post;
+  useLink?: boolean;
+}
+
+const PostContent = ({ post, useLink = true }: Props) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const cardContent = (
+    <div className="space-y-2">
+      <p className="text-lg font-semibold break-words">{post.title}</p>
+      <p
+        className={`text-sm break-words overflow-hidden  ${
+          !isExpanded ? "max-h-10" : "max-h-none"
+        }`}
+      >
+        {post.content}
+      </p>
+      {post.content && post.content.length > 100 && (
+        <button
+          className="text-cyan-500 text-sm mt-2 hover:text-cyan-600 transition-colors"
+          onClick={() => {
+            setIsExpanded(!isExpanded);
+          }}
+        >
+          {isExpanded ? "Read less" : "Read more"}
+        </button>
+      )}
+      {post.image && (
+        <div className="relative mx-auto max-w-[90%] h-fit max-h-[300px] overflow-hidden border-2 shadow-md rounded-2xl">
+          <Image
+            src={post.image}
+            alt="Post content"
+            width={200}
+            height={200}
+            className="w-full h-auto object-cover"
+            priority
+          />
+        </div>
+      )}
+    </div>
+  );
+
+  return useLink ? (
+    <Link href={`/posts/${post.id}`} className="block">
+      {cardContent}
+    </Link>
+  ) : (
+    cardContent
+  );
+};
+
+export default PostContent;
