b6f61cdc685a4ac19f3c1e757fb840448f349b90
baekmerve <baekmerve@gmail.com>
2025-03-13 02:29:33 +0900

    post header ui update

--- src/app/posts/postCard.tsx
+++ src/app/posts/postCard.tsx
@@ -33,54 +33,49 @@ export default function TestPostCard({
     <Card className="relative mx-auto w-[90%] overflow-hidden rounded-2xl shadow-lg border-2 dark:shadow-shadow px-4">
       {/* Overlay for blur effect and lock icon */}
       {!isLoggedIn && (
-        <div className="absolute inset-0 flex items-center justify-center bg-opacity-50 bg-bg/50 backdrop-blur-sm z-10">
-          <LockIcon />
+        <div className="absolute inset-0 flex  flex-col gap-2 items-center justify-center bg-black/40  bg-bg/50 backdrop-blur-sm z-10">
+          <LockIcon className="text-white size-7 mb-2" />
+          <p className=" text-white mt-2 font-semibold text-md">
+            로그인 후 전체 내용을 확인하세요
+          </p>
         </div>
       )}
-
       {/* POST HEADER */}
-      <CardHeader className="px-4">
-        <div className="flex items-center">
-          <Link href={`/profile/${post.author.username}`}>
-            <Avatar className="w-12 h-12 border-2">
+      <CardHeader className="px-6 py-3 border-b border-gray-200 dark:border-gray-700">
+        <div className="flex items-center justify-between">
+          {/* User Info */}
+          <Link
+            href={`/profile/${post.author.username}`}
+            className="flex items-center gap-4 group"
+          >
+            <Avatar className="w-12 h-12 border-2  ">
               <AvatarImage
                 src={post.author.image ?? "/avatar.avif"}
-                alt="user's avatar"
-                className="w-full h-full object-cover"
+                alt="User's avatar"
+                className="w-full h-full object-cover "
               />
             </Avatar>
-          </Link>
-          <div className="flex-1 min-w-0 p-2">
-            <div className="flex justify-between items-start">
-              <div className="w-full flex space-x-2 justify-between">
-                <div className="flex items-center space-x-2 text-sm">
-                  <Link
-                    href={`/profile/${post.author.username}`}
-                    className="font-semibold text-foreground text-base hover:scale-105"
-                  >
-                    {post.author.name}
-                  </Link>
-
-                  <Link
-                    href={`/profile/${post.author.username}`}
-                    className="hover:scale-105"
-                  >
-                    @{post.author.username}
-                  </Link>
-                  <span className="text-foreground">•</span>
-                  <span className="text-muted-foreground">
-                    {formatDistanceToNow(new Date(post.createdAt))} ago
-                  </span>
-                </div>
-                {currentUserId === post.authorId && (
-                  <DeleteItem
-                    itemId={post.id}
-                    deleteFunction={deletePost}
-                    title="Delete Post"
-                  />
-                )}
-              </div>
+            <div className="flex flex-col">
+              <p className="font-semibold text-foreground text-md group-hover:text-cyan-500 transition-colors">
+                {post.author.name || "Unknown User"}
+              </p>
+              <p className="text-muted-foreground text-sm group-hover:text-cyan-600 transition-colors">
+                @{post.author.username}
+              </p>
             </div>
+          </Link>
+
+          {/* Timestamp & Actions */}
+          <div className="flex items-center gap-4 text-muted-foreground text-xs">
+            <span>{formatDistanceToNow(new Date(post.createdAt))} ago</span>
+
+            {currentUserId === post.authorId && (
+              <DeleteItem
+                itemId={post.id}
+                deleteFunction={deletePost}
+                title="Delete Post"
+              />
+            )}
           </div>
         </div>
       </CardHeader>
